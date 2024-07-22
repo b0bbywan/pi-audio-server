b6cd45e6540f58dfa05623cab706d6e10355a9e5
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-07-22 23:58:59 +0200

    add cd autoplay with mpd

---
+++ stage3/07-cd/00-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+install -v -m 755 "files/mpc-cd.sh" "${ROOTFS_DIR}/usr/local/bin/mpc-cd.sh"
+install -v -m 644 "files/cd-mount.service" "${ROOTFS_DIR}/etc/systemd/system/cd-mount.service"
+install -v -m 644 "files/99-cd.rules" "${ROOTFS_DIR}/etc/udev/rules.d/99-cd.rules"
+
+on_chroot << EOF
+EOF
