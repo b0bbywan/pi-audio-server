86e201566f1ade19996b93d77cf6ccee223e2829
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-07-24 08:57:47 +0200

    create cue from cddb infos

--- stage3/07-cd/files/mpc-cd.sh
+++ stage3/07-cd/files/mpc-cd.sh
@@ -1,33 +1,63 @@
 #!/bin/bash
 
 ACTION="$1"
+DEV="/dev/cdrom"
+MPC="/usr/bin/mpc --host=/run/user/1000/mpd/socket"
+ENV_FILE="/tmp/cd_var.env"
+
+# Check if ACTION is provided
 if [[ -z ${ACTION} ]]; then
-	exit 1
+    logger "error: No ACTION specified"
+    exit 1
 fi
 
-DEV="/dev/cdrom"
-MPC="/usr/bin/mpc -p 6600"
+cdda_load() {
+    local num_tracks
+    num_tracks=$(udevadm info --query=property "${DEV}" | grep ID_CDROM_MEDIA_TRACK_COUNT_AUDIO | awk -F= '{ print $2 }')
+    if [[ -z ${num_tracks} ]]; then
+        logger "warning: Failed to detect CD tracks, adding whole cd"
+        ${MPC} add "cdda://"
+        return 1
+    fi
+
+    logger "info: CD with ${num_tracks} tracks detected"
+    for i in $(seq 1 "${num_tracks}"); do
+        ${MPC} add "cdda:///${i}"
+    done
+}
+
+cue_load() {
+    if [[ ! -f ${ENV_FILE} ]]; then
+        logger "warning: ${ENV_FILE} not found"
+        return 1
+    fi
+
+    source "${ENV_FILE}"
+    if [[ -z ${CUE_FILE} || ! -s ${CUE_FILE} ]]; then
+        logger "warning: Playlist empty or undefined"
+        return 1
+    fi
+    logger "info: Loading playlist from ${CUE_FILE}"
+    ${MPC} load "${CUE_FILE}"
+}
 
 do_mount() {
-	NUM_TRACK=${ID_CDROM_MEDIA_TRACK_COUNT_AUDIO}
-	if [[ -z ${NUM_TRACK} ]]; then
-		/usr/bin/logger "need udev to read track number"
-		NUM_TRACK=$(/sbin/udevadm info --query=property ${DEV} | /bin/grep ID_CDROM_MEDIA_TRACK_COUNT_AUDIO | /usr/bin/awk -F= '{ print $2 }')
-	fi
-	/usr/bin/logger "cd with ${NUM_TRACK} tracks detected"
+    logger "info: Clearing mpd queue"
+    ${MPC} clear
 
-	/usr/bin/logger "clearing mpd queue"
-	${MPC} clear
+    if ! cue_load; then
+        logger "info: Falling back to basic cdda"
+        cdda_load
+    fi
 
-	for i in $(seq 1 ${NUM_TRACK}); do
-		${MPC} add cdda:///${i}
-	done
-	${MPC} play
+    ${MPC} play
 }
 
 do_unmount() {
-	${MPC} stop
-	${MPC} -f "%position% %file%" playlist | /bin/grep cdda:// | /usr/bin/awk '{ print $1 }' | ${MPC} del
+    ${MPC} stop
+    logger "info: Clearing mpd queue before exit"
+    ${MPC} --format="%position% %file%" playlist | grep 'cdda://' | awk '{ print $1 }' | ${MPC} del
+    rm --force "${ENV_FILE}"
 }
 
 case "${ACTION}" in
@@ -36,5 +66,9 @@ case "${ACTION}" in
         ;;
     remove)
         do_unmount
+        ;;
+    *)
+        logger "error: Invalid ACTION '${ACTION}'"
+        exit 1
         ;;
 esac
