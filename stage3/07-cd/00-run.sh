08cde8958fa04f5d400dae4480dce5cb21f0c484
Mathieu Réquillart <mathieu.requillart@gmail.com>
2024-11-05 01:13:17 +0100

    new script to fetch cd infos

--- stage3/07-cd/00-run.sh
+++ stage3/07-cd/00-run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-wget https://raw.githubusercontent.com/johnlane/abcde/master/cddb-tool -O "${ROOTFS_DIR}/usr/local/bin/cddb-tool"
-chown root:root "${ROOTFS_DIR}/usr/local/bin/cddb-tool" && chmod 755 "${ROOTFS_DIR}/usr/local/bin/cddb-tool"
 install -v -m 755 -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.cddb"
+install -v -m 755 "files/mbdiscid.pl" "${ROOTFS_DIR}/usr/local/bin/mbdiscid.pl"
 install -v -m 755 "files/cd-cue.sh" "${ROOTFS_DIR}/usr/local/bin/cd-cue.sh"
 install -v -m 755 "files/mpc-cd.sh" "${ROOTFS_DIR}/usr/local/bin/mpc-cd.sh"
 install -v -m 644 "files/cd-mount.service" "${ROOTFS_DIR}/etc/systemd/system/cd-mount.service"
--- stage3/07-cd/files/cd-cue.sh
+++ stage3/07-cd/files/cd-cue.sh
@@ -1,101 +1,73 @@
 #!/bin/bash
 
-CDDB_URL="http://gnudb.gnudb.org/~cddb/cddb.cgi"
-OPTS="3 $(whoami) $(hostname)"
-CACHE_LOCATION="/home/$(id --user --name 1000)/.cddb"
 ENV_FILE="/tmp/cd_var.env"
+MB_URL="https://musicbrainz.org/ws/2"
+COVER_ART_URL="https://coverartarchive.org/release"
+CACHE_LOCATION="/home/$(id --user --name 1000)/.cddb"
+
+getDiscId() {
+    ./mbdiscid.pl || { logger "Error: Failed to fetch Disc ID"; exit 1; }
+}
+
+getRelease() {
+    local disc_id="$1"
+    res=$(curl -s "${MB_URL}/discid/${disc_id}?inc=artists+recordings+release-groups&fmt=json") || {
+        logger "Error: Failed to fetch release data"; exit 1; }
+
+    echo "$res" | jq -c '.releases[0]'
+}
+
+getCoverArtFromMBId() {
+    local mb_id="$1"
+    local cover_file="$2"
+    wget --quiet "${COVER_ART_URL}/${mb_id}/front" -O "${cover_file}"
+}
 
 createPlaylist() {
     local cue_file="$1"
-    local data="$2"
+    local release="$2"
+    local disc_cover="$3"
     install --mode=644 --owner=1000 --group=1000 /dev/null "${cue_file}"
 
-    IFS=$'\n' read -r -d '' -a lines <<< "$data"
-    local full_title=""
-    local tracks=()
-    for line in "${lines[@]}"; do
-        case "$line" in
-            DTITLE=*)
-                full_title=$(echo "${line#DTITLE=}" | sed 's/\r//')
-                ;;
-            TTITLE*)
-                title=$(echo "${line#TTITLE*=}" | sed 's/\r//')
-                tracks+=("${title}")
-                ;;
-        esac
-    done
-
-    if [[ -z $full_title ]]; then
-        logger "error: Unable to create playlist with data"
+    if [[ -z "$release" ]]; then
+        logger "Error: No release data found."
         return 1
     fi
 
-    local disc_performer="${full_title%% / *}"
-    local disc_title="${full_title##* / }"
+    disc_title=$(echo "$release" | jq -r '.title')
+    disc_performer=$(echo "$release" | jq -r '.["artist-credit"][0].name')
+    disc_year=$(echo "$release" | jq -r '.date')
 
     # Write CUE file
     {
+        echo "REM DATE \"${disc_year}\""
+        echo "REM COVER \"${disc_cover}\""
         echo "PERFORMER \"${disc_performer}\""
         echo "TITLE \"${disc_title}\""
-        for i in "${!tracks[@]}"; do
-            echo "FILE \"cdda:///$((i+1))\" WAVE"
-            echo "  TRACK $(printf "%02d" $((i+1))) AUDIO"
-            echo "    TITLE \"${tracks[$i]}\""
+        track_number=1
+        echo "$release" | jq -rc '.media[0].tracks | sort_by(.position) | .[].title' | while IFS= read -r track; do
+            echo "FILE \"cdda:///${track_number}\" WAVE"
+            echo "  TRACK $(printf "%02d" "${track_number}") AUDIO"
+            echo "    TITLE \"${track}\""
+            ((track_number++))
         done
     } > "${cue_file}"
     logger "info: CUE file generated: ${cue_file}"
 }
 
-queryDB() {
-    local cd_id="$1"
-    local cd_from_db
-    local cddb_id
-
-    cd_from_db=$(cddb-tool query "$CDDB_URL" $OPTS "$cd_id")
-    cddb_id=$(echo "$cd_from_db" | head -1 | awk '{print $2 " " $3}')
-    if [[ $cddb_id == "No match" ]]; then
-        logger "error: No match found for CD ID ${cd_id} in ${CDDB_URL}"
-        return 1
-    fi
-    cddb-tool read "$CDDB_URL" $OPTS "$cddb_id"
-}
-
-getCachedOrQueryData() {
-    local cd_id="$1"
-    local cache_file="$2"
-    # Check if data is cached
-    if [[ -f "${cache_file}" && -s "${cache_file}" ]]; then
-        logger "info: Reading ${cd_id} from ${cache_file}"
-        cat "${cache_file}"
-        return 0
-    fi
-
-    # Query the CDDB if not cached
-    if ! infos=$(queryDB "${cd_id}"); then
-        return 1
-    fi
-    logger "info: match found in CDDB"
-    echo "${infos}" | tee "${cache_file}"
-}
-
 rm --force "${ENV_FILE}"
 
-CD_ID=$(cd-discid /dev/sr0)
-NAME_FILE=$(echo "${CD_ID}" | awk '{print $1}')
-
-cue_file="${CACHE_LOCATION}/${NAME_FILE}.cue"
-
-# Check if the CUE file does not exist or is empty
+disc_id=$(getDiscId)
+#disc_id="VBp.rfhj1s2PoGNYGjpzmsk_vV0-"
+cue_file="${CACHE_LOCATION}/${disc_id}.cue"
 if [[ ! -s "${cue_file}" ]]; then
-    if ! data=$(getCachedOrQueryData "${CD_ID}" "${CACHE_LOCATION}/${NAME_FILE}"); then
-        logger "error: Failed to get data for ${NAME_FILE}"
-        exit 1
+    release=$(getRelease "${disc_id}")
+    cover_file="${CACHE_LOCATION}/${disc_id}.jpg"
+    if [[ ! -s ${cover_file} ]]; then
+        getCoverArtFromMBId "$(echo "${release}" | jq -r '.id')" "${cover_file}"
     fi
-    if ! createPlaylist "${cue_file}" "${data}"; then
-        logger "error: Failed to create playlist for ${NAME_FILE}"
-        exit 1
-    fi
+    createPlaylist "${cue_file}" "${release}" "${cover_file}"
 fi
 
 logger "info: Playlist found ${cue_file}"
-echo "CUE_FILE=${cue_file}" > "${ENV_FILE}"
+echo "$cue_file" > "${ENV_FILE}"
