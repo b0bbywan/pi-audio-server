2fb25fe2d3d677fbe13709b800a69aaf1e9e4282
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-01-09 19:05:09 +0100

    remove wip

--- stage-audio/07-cd/00-run.sh
+++ stage-audio/07-cd/00-run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-install -v -m 755 -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.cddb"
-install -v -m 755 "files/mbdiscid.pl" "${ROOTFS_DIR}/usr/local/bin/mbdiscid.pl"
-install -v -m 755 "files/cd-cue.sh" "${ROOTFS_DIR}/usr/local/bin/cd-cue.sh"
 install -v -m 755 "files/mpc-cd.sh" "${ROOTFS_DIR}/usr/local/bin/mpc-cd.sh"
 install -v -m 644 "files/cd-mount.service" "${ROOTFS_DIR}/etc/systemd/system/cd-mount.service"
 install -v -m 644 "files/99-cd.rules" "${ROOTFS_DIR}/etc/udev/rules.d/99-cd.rules"
