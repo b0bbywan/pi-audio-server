c430845d26c804888647b17413df2e845ab30f4b
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-01-09 20:08:08 +0100

    update trixie

--- stage-audio/00-lite-raspios/00-run.sh
+++ stage-audio/00-lite-raspios/00-run.sh
@@ -3,17 +3,19 @@
 on_chroot << EOF
 	apt-get purge -y 'linux-headers-*' || true
 
-	systemctl disable dphys-swapfile || true
-	systemctl mask dphys-swapfile || true
+	systemctl disable rpi-swap || true
+	systemctl mask rpi-swap || true
 
 	systemctl disable nfs-common rpcbind || true
 
 	apt-get purge -y \
+	apt-listchanges \
 	build-essential gdb manpages-dev \
 	python3-gpiozero python3-rpi.gpio python3-spidev \
 	v4l-utils \
 	nfs-common rpcbind \
-	dphys-swapfile \
+	rpi-connect-lite \
+	rpi-swap rpi-loop-utils \
 	rpicam-apps-lite || true
 
 	apt-get autoremove -y
