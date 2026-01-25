67a8d11e8c88a7171e2b92e2efb975a954dcbbb4
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-01-25 03:16:18 +0100

    lite

--- stage-audio/00-lite-raspios/00-run.sh
+++ stage-audio/00-lite-raspios/00-run.sh
@@ -1,22 +1,19 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+
 	apt-get purge -y 'linux-headers-*' || true
 
-	systemctl disable rpi-swap || true
-	systemctl mask rpi-swap || true
-
-	systemctl disable nfs-common rpcbind || true
-
 	apt-get purge -y \
-	apt-listchanges \
-	build-essential gdb manpages-dev \
-	python3-gpiozero python3-rpi.gpio python3-spidev \
-	v4l-utils \
-	nfs-common rpcbind \
-	rpi-connect-lite \
-	rpi-swap rpi-loop-utils \
-	rpicam-apps-lite || true
+		man-db manpages \
+		apt-listchanges \
+		build-essential gdb manpages-dev \
+		python3-gpiozero python3-rpi.gpio python3-spidev \
+		v4l-utils \
+		nfs-common rpcbind \
+		rpi-connect-lite \
+		rpi-swap rpi-loop-utils \
+		rpicam-apps-lite || true
 
 	apt-get autoremove -y
 EOF
